352ab10cd1dc14db89822733477d54fffe78e3ff
scienceopen <scienceopen@users.noreply.github.com>
2014-10-29 17:18:15 -0400

    plots

--- satplot.py
+++ satplot.py
@@ -1,67 +1,146 @@
 #!/usr/bin/env python3
-from ephem import readtle,Observer,city
+from pdb import set_trace
+from ephem import readtle,Observer
 import simplekml as skml
 from os.path import expanduser
-from pdb import set_trace
-from numpy import degrees,empty_like,nan
-from pandas import date_range
+from numpy import degrees,nan,isnan
+from pandas import date_range, DataFrame
 from matplotlib.pyplot import figure,show
 from matplotlib.ticker import MultipleLocator
+from dateutil.parser import parse
+from re import search
 
-def main(tle1,tle2,kmlfn):
-    kmlfn = expanduser(kmlfn)    
-#%% preallocation    
-    forNhours = 12
-    everyNminutes = 15    
-    dates = date_range('2014-10-29T02:00:00',
-                       periods=forNhours*60/everyNminutes+1,
-                       freq=str(everyNminutes)+'T', tz='UTC')   
-    lat = empty_like(dates,dtype=float)
-    lon = empty_like(lat); alt_m = empty_like(lat)
-    az = empty_like(lat); el = empty_like(lat)
-#%% do work    
-    obs = Observer()    
-    obs.lat = 64.8; obs.lon = -147.7
-    #obs = city('Seattle')
-    sat = readtle('mySat',tle1, tle2)
-    for i,d in enumerate(dates):
-        obs.date = d
-        sat.compute(obs)
-        lat[i],lon[i],alt_m[i] = degrees(sat.sublat), degrees(sat.sublong), sat.elevation
-        az[i], el[i] = degrees(sat.az), degrees(sat.alt)
-        
-    az[el<0] = nan; el[el<0] = nan
-    
-    
-    ax = figure().gca()
-    ax.plot(lon,lat,marker='.')
-    ax.set_ylabel('lat [deg.]')
-    ax.set_xlabel('lon [deg.]')
-    ax.set_xlim(-180,180)
-    ax.set_ylim(-90,90)
-    ax.set_title(str(dates[0]) + ' to ' + str(dates[-1]))
-    ax.grid(True)
-    ax.yaxis.set_major_locator(MultipleLocator(15))
-    ax.yaxis.set_minor_locator(MultipleLocator(5))
-    ax.xaxis.set_major_locator(MultipleLocator(30))
-    ax.xaxis.set_minor_locator(MultipleLocator(5))
-    
-    ax = figure().gca()
-    ax.plot(az,el,marker='.')
-    ax.set_xlabel('azimuth [deg.]')
-    ax.set_ylabel('elevation [deg.]')
+def main(tlefn,date,kmlfn):
+    obs = Observer()
+    obs.lat = str(65); obs.lon = str(-148)
+    obs.elevation=0
+
+#%% preallocation
+    if date is None:
+        satnum = [None]
+        forNhours = 12
+        everyNminutes = 15
+        dates = date_range('2014-10-29T02:00:00',
+                           periods=forNhours*60/everyNminutes+1,
+                           freq=str(everyNminutes)+'T', tz='UTC')
+        data = DataFrame(index=dates,columns=['az','el','lat','lon','alt','srange'])
+        sat = readtle('mySat',tle1, tle2)
+        for i,d in enumerate(dates):
+            obs.date = d
+            sat.compute(obs)
+            data.ix[i,'lat'] = degrees(sat.sublat)
+            data.ix[i,'lon'] = degrees(sat.sublong)
+            data.ix[i,'alt'] = sat.elevation
+            data.ix[i,'az'], data.ix[i,'el'] = degrees(sat.az), degrees(sat.alt)
+            data.ix[i,'srange'] = sat.range
+    else:
+        dates = [parse(date)]
+        obs.date = dates[0]
+        sats,satnum = loadTLE(tlefn)
+        data = DataFrame(index=satnum,columns=['az','el','lat','lon','alt','srange'])
+
+        for i,s in enumerate(sats):
+            si = satnum[i]
+            s.compute(obs)
+            data.ix[si,'lat'] = degrees(s.sublat)
+            data.ix[si,'lon'] = degrees(s.sublong)
+            data.ix[si,'alt'] = s.elevation
+            data.ix[si,'az'] = degrees(s.az)
+            data.ix[si,'el'] = degrees(s.alt)
+            data.ix[si,'srange'] = s.range
+
+    belowhoriz = data['el']<0
+    data.ix[belowhoriz,['az','el','srange']] = nan
+#%% write kml
+    dokml(belowhoriz,data['lat'],data['lon'],data['alt'],
+          obs.lat.real,obs.lon.real,obs.elevation.real,
+          kmlfn)
+#%% plot
+    doplot(data['lat'],data['lon'],data['az'],data['el'],dates,satnum)
+
+def loadTLE(filename):
+    """ Loads a TLE file and creates a list of satellites.
+    http://blog.thetelegraphic.com/2012/gps-sattelite-tracking-in-python-using-pyephem/
+    """
+    #pat = '(?<=PRN)\d\d'
+    with open(filename) as f:
+        satlist = []; prn = []
+        l1 = f.readline()
+        while l1:
+            l2 = f.readline()
+            l3 = f.readline()
+            sat = readtle(l1,l2,l3)
+            satlist.append(sat)
+
+            prn.append(int(search(r'(?<=PRN)\s*\d\d',sat.name).group()))
+            l1 = f.readline()
+
+    print(str(len(satlist)) + ' satellites loaded into list')
+    return satlist,prn
+
+def dokml(belowhoriz,lat,lon,alt_m,lat0,lon0,alt0,kmlfn):
+
+    Npt = lat.size
+    if kmlfn is not None:
+        kmlfn = expanduser(kmlfn)
+        print('writing KML to ' + kmlfn)
+        kml1d = skml.Kml()
+        for i in range(Npt):
+            if not belowhoriz[i]:
+                linestr = kml1d.newlinestring(name='')
+                linestr.coords = [(lon0,   lat0,   alt0),
+                                  (lon[i], lat[i], alt_m[i])]
+                linestr.altitudemode = skml.AltitudeMode.relativetoground
+        kml1d.save(kmlfn)
+
+def doplot(lat,lon,az,el,dates,satnum):
+    Npt = lat.size
+    ax1 = figure(1).gca()
+    ax1.set_ylabel('lat [deg.]')
+    ax1.set_xlabel('lon [deg.]')
+    ax1.set_xlim(-180,180)
+    ax1.set_ylim(-90,90)
+    ax1.set_title('Latitude & Longitude starting\n' + str(dates[0]))
+    ax1.grid(True)
+    ax1.yaxis.set_major_locator(MultipleLocator(15))
+    ax1.yaxis.set_minor_locator(MultipleLocator(5))
+    ax1.xaxis.set_major_locator(MultipleLocator(30))
+    ax1.xaxis.set_minor_locator(MultipleLocator(5))
+
+    ax2 = figure(2).gca()
+    ax2.set_xlabel('azimuth [deg.]')
+    ax2.set_ylabel('elevation [deg.]')
+    ax2.grid(True)
+    ax2.set_xlim(0,360)
+    ax2.set_ylim(0,90)
+
+
+    if len(satnum) ==1: #vs time case
+        ax1.plot(lon,lat,marker='.')
+        ax2.plot(az,el,marker='.')
+        ax2.set_title('Azimuth & Elevation starting\n' + str(dates[0]))
+        for i in range(Npt):
+            if not isnan(az[i]):
+                ax2.text(az[i]+3,el[i],dates[i].strftime('%H:%M'),fontsize=8)
+    elif len(dates)==1: # lots of sats case
+        ax1.plot(lon,lat,marker='.',linestyle='')
+        ax2.plot(az,el,marker='.',linestyle='')
+        ax2.set_title('Azimuth & Elevation by PRN at\n' + str(dates[0]))
+        for s in satnum:
+            ax1.text(lon[s]+3,lat[s]+3,s,fontsize=10)
+            if not isnan(az[s]):
+                ax2.text(az[s]+3,el[s],s,fontsize=10)
+
+
     show()
-    
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='converts satellite position into KML for Google Earth viewing')
-    p.add_argument('kmlfn',help='filename to save KML to',type=str,default=None)
-    p.add_argument('line1',help='paste line 1 of TLE',type=str,nargs=9)    
-    p.add_argument('line2',help='paste line 2 of TLE',type=str,nargs=8)
+    p.add_argument('tlefn',help='file with TLE to parse',type=str)
+    p.add_argument('date',help='time to plot YYYY-mm-ddTHH:MM:SSZ')
+    p.add_argument('-k','--kmlfn',help='filename to save KML to',type=str,default=None)
     args = p.parse_args()
 
-    
-    tle1 = ' '.join(args.line1)
-    tle2 = ' '.join(args.line2)
-    main(tle1,tle2,args.kmlfn)
-    
+    main(args.tlefn,args.date,args.kmlfn)
+

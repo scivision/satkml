a998bfd0088ca549261ced8e92f142d97a693bfc
Michael Hirsch <scienceopen@users.noreply.github.com>
2015-05-13 16:33:09 -0400

    Delete .datajoy.py

--- .datajoy.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is a hack for Datajoy.com, it is not for use on your own PC
-"""
-
-import os
-os.system("sudo conda install ephem basemap")
-
-
-os.system('python satplot.py gps-ops.txt 2014-10-29T16:00:00Z -p')

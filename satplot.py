1eeabc5fcebd3fba23d6f5e77bc28022556fe784
scienceopen <scienceopen@users.noreply.github.com>
2015-05-13 13:27:35 -0400

    fixing up multi-time case

--- satplot.py
+++ satplot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #from pdb import set_trace
+from __future__ import division
 from ephem import readtle,Observer
 from os.path import expanduser
 from numpy import degrees,nan,isnan,arange
@@ -11,19 +12,24 @@ from re import search
 
 def main(tlefn,date,kmlfn,obslla,satreq, showplot):
     obs = Observer()
-    obs.lat = str(obslla[0]); obs.lon = str(obslla[1])
-    obs.elevation=0
-
+    try:
+        obs.lat = str(obslla[0]); obs.lon = str(obslla[1])
+        obs.elevation=str(obslla[2])
+    except ValueError:
+        print('observation location not specified. defaults to lat=0, lon=0')
 #%% preallocation
     if satreq is not None:
         forNhours = 12
         everyNminutes = 15
         dates = date_range(parse(date),
                            periods=forNhours*60/everyNminutes+1,
-                           freq=str(everyNminutes)+'T', tz='UTC')
+                           freq=str(everyNminutes)+'T')#, tz='UTC')
         data = DataFrame(index=dates,columns=['az','el','lat','lon','alt','srange'])
         sats,satnum = loadTLE(tlefn)
-        sat = data.ix[satreq,:]
+        try:
+            sat = sats[satnum.index(satreq)]
+        except ValueError as e:
+            exit('i had trouble finding satellite {}.   {}'.format(satreq,e))
         satnum = satreq
         for i,d in enumerate(dates):
             obs.date = d
@@ -56,7 +62,7 @@ def main(tlefn,date,kmlfn,obslla,satreq, showplot):
         doplot(data['lat'],data['lon'],data['az'],data['el'],dates,satnum)
 #%% write kml
     dokml(belowhoriz,data['lat'],data['lon'],data['alt'],
-          obslla, kmlfn,satnum)
+          obs, kmlfn,satnum)
 #%% fancy plot
     if showplot:
         fancyplot(data['lat'].values,data['lon'].values,dates,satnum)
@@ -105,10 +111,10 @@ def loadTLE(filename):
             prn.append(int(search(r'(?<=PRN)\s*\d\d',sat.name).group()))
             l1 = f.readline()
 
-    print(str(len(satlist)) + ' satellites loaded into list')
+    print('{} satellites loaded into list'.format(len(satlist)))
     return satlist,prn
 
-def dokml(belowhoriz,lat,lon,alt_m,lla,kmlfn,satnum):
+def dokml(belowhoriz,lat,lon,alt_m,obs,kmlfn,satnum):
     if kmlfn is not None:
         try:
             import simplekml as skml
@@ -118,7 +124,7 @@ def dokml(belowhoriz,lat,lon,alt_m,lla,kmlfn,satnum):
             for s in satnum:
                 if not belowhoriz[s]:
                     linestr = kml1d.newlinestring(name=str(s))
-                    linestr.coords = [(lla[1], lla[0], lla[2]),
+                    linestr.coords = [(obs.lon, obs.lat, obs.elevation),
                                       (lon[s], lat[s], alt_m[s])]
                     linestr.altitudemode = skml.AltitudeMode.relativetoground
             kml1d.save(kmlfn)
@@ -127,6 +133,8 @@ def dokml(belowhoriz,lat,lon,alt_m,lla,kmlfn,satnum):
 
 
 def doplot(lat,lon,az,el,dates,satnum):
+    polar =True
+
     fg = figure()
     ax1 = fg.gca()
     ax1.set_ylabel('lat [deg.]')
@@ -140,22 +148,28 @@ def doplot(lat,lon,az,el,dates,satnum):
     ax1.xaxis.set_major_locator(MultipleLocator(30))
     ax1.xaxis.set_minor_locator(MultipleLocator(5))
 
-    ax2 = figure().gca()
-    ax2.set_xlabel('azimuth [deg.]')
-    ax2.set_ylabel('elevation [deg.]')
+    if polar:
+        ax2=figure().gca(polar=True)
+        ax2.set_theta_zero_location('N')
+        ax2.set_theta_direction(-1)
+    else:
+        ax2 = figure().gca()
+        ax2.set_xlabel('azimuth [deg.]')
+        ax2.set_ylabel('elevation [deg.]')
+        ax2.set_xlim(0,360)
+        ax2.set_ylim(0,90)
+
     ax2.grid(True)
-    ax2.set_xlim(0,360)
-    ax2.set_ylim(0,90)
 
 
-    if len(satnum) ==1: #vs time case
+    if len(dates) !=1: #vs time case
         ax1.plot(lon,lat,marker='.')
         ax2.plot(az,el,marker='.')
         ax2.set_title('Azimuth & Elevation starting\n' + str(dates[0]))
         for i in range(lat.size):
             if not isnan(az[i]):
                 ax2.text(az[i]+3,el[i],dates[i].strftime('%H:%M'),fontsize=8)
-    elif len(dates)==1: # lots of sats case
+    else: # lots of sats case
         ax1.plot(lon,lat,marker='.',linestyle='')
         ax2.plot(az,el,marker='.',linestyle='')
         ax2.set_title('Azimuth & Elevation by PRN at\n' + str(dates[0]))
@@ -171,11 +185,11 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='converts satellite position into KML for Google Earth viewing')
     p.add_argument('tlefn',help='file with TLE to parse',type=str)
-    p.add_argument('date',help='time to plot YYYY-mm-ddTHH:MM:SSZ')
+    p.add_argument('date',help='time to plot YYYY-mm-ddTHH:MM:SSZ',type=str)
     p.add_argument('-p','--plot',help='show plots',action='store_true')
-    p.add_argument('-l','--lla',help='WGS84 lat lon [degrees] alt [meters] of observer',nargs=3,default=(65,-148,0))
+    p.add_argument('-l','--lla',help='WGS84 lat lon [degrees] alt [meters] of observer',nargs=3,default=(None,None,None))
     p.add_argument('-k','--kmlfn',help='filename to save KML to',type=str,default=None)
-    p.add_argument('--sat',help='satellite you want to pick from file',type=str,default=None)
+    p.add_argument('--sat',help='satellite you want to pick from file',type=int,default=None)
     a = p.parse_args()
 
     data = main(a.tlefn,a.date,a.kmlfn,a.lla,a.sat,a.plot)

ce8c83db68b58c8db7341b09765b07332082638c
Michael <hirsch617@gmail.com>
2015-05-14 01:56:33 -0400

    multi-time

--- satplot.py
+++ satplot.py
@@ -6,92 +6,78 @@ from __future__ import division
 from ephem import readtle,Observer
 from os.path import expanduser
 from numpy import degrees,nan,isfinite,arange,radians
-from pandas import date_range, DataFrame
+from pandas import date_range, DataFrame,Panel
 from matplotlib.pyplot import figure,show
 from matplotlib.ticker import MultipleLocator
 from dateutil.parser import parse
 from re import search
 
-forNhours = 24
-everyNminutes = 15
-
-def loopsat(tlefn,date,kmlfn,obslla,satreq):
+def loopsat(tlefn,dates,kmlfn,obslla):
     obs = setupobs(obslla)
-#%% preallocation
-    if satreq is not None:
-        dates = date_range(parse(date),
-                           periods=forNhours*60/everyNminutes+1,
-                           freq=str(everyNminutes)+'T')#, tz='UTC')
-        data = DataFrame(index=dates,columns=['az','el','lat','lon','alt','srange'])
-        sats,satnum = loadTLE(tlefn)
-        try:
-            sat = sats[satnum.index(satreq)]
-        except ValueError as e:
-            exit('i had trouble finding satellite {}.   {}'.format(satreq,e))
-        satnum = satreq
-        for d in dates:
-            obs.date = d
-            sat.compute(obs)
-            data.at[d,['lat','lon','alt']] = degrees(sat.sublat), degrees(sat.sublong), sat.elevation
-            data.at[d,['az','el','srange']] = degrees(sat.az), degrees(sat.alt),sat.range
 
-        belowhoriz = data['el']<0
-        data.ix[belowhoriz,['az','el','srange']] = nan
-    else:
-        dates = [parse(date)] #list for plotting
-        data,satnum,belowhoriz = compsat(tlefn,obs,dates)
+    data,belowhoriz = compsat(tlefn,obs,dates)
 
-    return data,dates,satnum,belowhoriz,obs
+    return data,obs,belowhoriz
 
 def setupobs(lla):
     obs = Observer()
     try:
-        obs.lat = str(lla[0]); obs.lon = str(lla[1]); obs.elevation=str(lla[2])
+        obs.lat = str(lla[0]); obs.lon = str(lla[1]); obs.elevation=float(lla[2])
     except ValueError:
         print('observation location not specified. defaults to lat=0, lon=0')
     return obs
 
 def compsat(tlefn,obs,dates):
-    obs.date = dates[0]
+    cols = ['az','el','lat','lon','alt','srange']
     sats,satnum = loadTLE(tlefn)
-    data = DataFrame(index=satnum,columns=['az','el','lat','lon','alt','srange'])
 
-    for i,s in enumerate(sats):
-        si = satnum[i]
-        s.compute(obs)
-        data.at[si,['lat','lon','alt']] = degrees(s.sublat), degrees(s.sublong), s.elevation
-        data.at[si,['az','el','srange']] = degrees(s.az), degrees(s.alt), s.range
+    data = Panel(items=dates, major_axis=satnum, minor_axis=cols)
+    for d in dates:
+        obs.date = d
 
-    belowhoriz = data['el']<0
-    data.ix[belowhoriz,['az','el','srange']] = nan
+        df = DataFrame(index=satnum,columns=cols)
+        for i,s in enumerate(sats):
+            si = satnum[i]
+            s.compute(obs)
+            df.at[si,['lat','lon','alt']] = degrees(s.sublat), degrees(s.sublong), s.elevation
+            df.at[si,['az','el','srange']] = degrees(s.az), degrees(s.alt), s.range
 
-    return data,satnum,belowhoriz
+        belowhoriz = df['el']<0
+        df.ix[belowhoriz,['az','el','srange']] = nan
 
-def fancyplot(lat,lon,dates,satnum):
+        data[d] = df
+
+    return data,belowhoriz
+
+def fancyplot(data):
     try:
         from mpl_toolkits.basemap import Basemap
     except ImportError as e:
         print('could not make fancy plot.  {}'.format(e))
         return
-    #lon and lat cannot be pandas Series, must be values
-    ax= figure().gca()
-    m = Basemap(projection='merc',
-                  llcrnrlat=-80, urcrnrlat=80,
-                  llcrnrlon=-180,urcrnrlon=180,
-                  lat_ts=20,
-                  resolution='c')
 
-    m.drawcoastlines()
-    m.drawcountries()
-    m.drawmeridians(arange(0,360,30))
-    m.drawparallels(arange(-90,90,30))
-    x,y = m(lon,lat)
-    m.plot(x,y,'o',color='#aaaaff',markersize=14)
-    ax.set_title('GPS constellation at\n' + str(dates[0]))
-    if not isinstance(satnum,list):
-        satnum = [satnum]
-    for s,xp,yp in zip(satnum,x,y):
-        ax.text(xp,yp,s,ha='center',va='center',fontsize=11)
+    dates = data.items.values
+    satnum = data.major_axis
+    #lon and lat cannot be pandas Series, must be values
+    for d in data:
+        lat= data[d]['lat'].values; lon= data[d]['lon'].values
+        ax= figure(3).gca()
+        m = Basemap(projection='merc',
+                      llcrnrlat=-80, urcrnrlat=80,
+                      llcrnrlon=-180,urcrnrlon=180,
+                      lat_ts=20,
+                      resolution='c')
+
+        m.drawcoastlines()
+        m.drawcountries()
+        m.drawmeridians(arange(0,360,30))
+        m.drawparallels(arange(-90,90,30))
+        x,y = m(lon,lat)
+        m.plot(x,y,'o',color='#aaaaff',markersize=14)
+        ax.set_title('GPS constellation from {} to {}'.format(dates[0],dates[-1]))
+
+        for s,xp,yp in zip(satnum,x,y):
+            ax.text(xp,yp,s,ha='center',va='center',fontsize=11)
 
 
 def loadTLE(filename):
@@ -113,34 +99,46 @@ def loadTLE(filename):
 
     return satlist,prn
 
-def dokml(belowhoriz,lat,lon,alt_m,obs,kmlfn,satnum):
+def dokml(belowhoriz,data,obs,kmlfn):
+    # TODO: make this work with multiple times properly (show animated paths)
     if kmlfn is not None:
-        try:
-            import simplekml as skml
-            kmlfn = expanduser(kmlfn)
-            print('writing KML to ' + kmlfn)
-            kml1d = skml.Kml()
-            for s in satnum:
-                if not belowhoriz[s]:
-                    linestr = kml1d.newlinestring(name=str(s))
-                    linestr.coords = [(obs.lon, obs.lat, obs.elevation),
-                                      (lon[s], lat[s], alt_m[s])]
-                    linestr.altitudemode = skml.AltitudeMode.relativetoground
-            kml1d.save(kmlfn)
-        except Exception as e:
-            print('unable to write KML. Do you have simplekml package installed? ' + str(e))
+      try:
+          for d in data:
+              lat= data[d]['lat']; lon= data[d]['lon']
+              alt_m = data[d]['alt']
+              satnum = data[d].index
+
+              import simplekml as skml
+              kmlfn = expanduser(kmlfn)
+              kml1d = skml.Kml()
+              for s in satnum:
+                  if not belowhoriz[s]:
+                      linestr = kml1d.newlinestring(name=str(s))
+                      linestr.coords = [(obs.lon, obs.lat, obs.elevation),
+                                        (lon[s], lat[s], alt_m[s])]
+                      linestr.altitudemode = skml.AltitudeMode.relativetoground
+              kml1d.save(kmlfn)
+      except Exception as e:
+          print('unable to write KML. Do you have simplekml package installed? ' + str(e))
 
 
-def doplot(lat,lon,az,el,dates,satnum):
-    polar = True
+def doplot(data):
+  polar = True
 
-    fg = figure()
+  dates = data.items
+  satnum= data.major_axis
+
+  for s in satnum:
+    az = data.ix[:,s,'az'].values;  el = data.ix[:,s,'el'].values
+    lat= data.ix[:,s,'lat'].values; lon= data.ix[:,s,'lon'].values #need .values for indexing
+
+    fg = figure(1)
     ax1 = fg.gca()
     ax1.set_ylabel('lat [deg.]')
     ax1.set_xlabel('lon [deg.]')
     ax1.set_xlim(-180,180)
     ax1.set_ylim(-90,90)
-    ax1.set_title('Latitude & Longitude starting\n' + str(dates[0]))
+    ax1.set_title('Latitude & Longitude from\n{} to {}'.format(dates[0], dates[-1]))
     ax1.grid(True)
     ax1.yaxis.set_major_locator(MultipleLocator(15))
     ax1.yaxis.set_minor_locator(MultipleLocator(5))
@@ -151,7 +149,7 @@ def doplot(lat,lon,az,el,dates,satnum):
         azoffs = 0#radians(3)
         az = radians(az.astype(float))
         el = 90-el.astype(float)
-        ax2=figure().gca(polar=True)
+        ax2=figure(2).gca(polar=True)
         ax2.plot(az,el, marker='.',linestyle='')
         ax2.set_theta_zero_location('N')
         ax2.set_theta_direction(-1)
@@ -161,7 +159,7 @@ def doplot(lat,lon,az,el,dates,satnum):
         ax2.set_yticklabels(yLabel)
     else:
         azoffs=3
-        ax2 = figure().gca()
+        ax2 = figure(2).gca()
         ax2.plot(az,el,marker='.',linestyle='')
         ax2.set_xlabel('azimuth [deg.]')
         ax2.set_ylabel('elevation [deg.]')
@@ -170,44 +168,51 @@ def doplot(lat,lon,az,el,dates,satnum):
 
     ax2.grid(True)
 
-
     ax1.plot(lon,lat,marker='.',linestyle='')
+    ax2.set_title('Azimuth & Elevation by PRN from\n{} to {}'.format(dates[0],dates[-1]))
 
-    if len(dates) !=1: #vs time case
-        ax2.set_title('Azimuth & Elevation starting\n' + str(dates[0]))
-        for i in range(lat.size):
-            if isfinite(az[i]):
-                ax2.text(az[i]+azoffs, el[i],
-                         dates[i].strftime('%H:%M'),fontsize='small')
-    else: # lots of sats case
-        ax2.set_title('Azimuth & Elevation by PRN at\n' + str(dates[0]))
-        for s in satnum:
-            ax1.text(lon[s]+3, lat[s]+3,s,fontsize='small')
-            if isfinite(az[s]):
-                ax2.text(az[s]+azoffs, el[s],
-                         s,fontsize='small')
+    if len(dates)<6: #don't want overcrowded plot
+        for i,d in enumerate(dates):
+                pl = '{} {}'.format(s,d.strftime('%H:%M'))
+                ax1.text(lon[i]+3, lat[i]+3,pl,fontsize='x-small')
+                if isfinite(az[i]):
+                    ax2.text(az[i]+azoffs, el[i],pl,fontsize='x-small')
+    else: #just label first point
+        #too much text for all satellites and all times (maybe label once per line)
+        ax1.text(lon[0]+3, lat[0]+3,s,fontsize='small')
+        if isfinite(az[0]):
+            ax2.text(az[0]+azoffs, el[0],
+                 s,fontsize='small')
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='converts satellite position into KML for Google Earth viewing')
     p.add_argument('tlefn',help='file with TLE to parse',type=str)
-    p.add_argument('date',help='time to plot YYYY-mm-ddTHH:MM:SSZ',type=str)
+    p.add_argument('date',help='start/stop time to start 24 hour plot YYYY-mm-ddTHH:MM:SSZ',nargs='+',type=str)
+    p.add_argument('-T','--period',help='time interval (MINUTES) to compute sats. position (default=15 min)',type=str,default='15')
     p.add_argument('--noplot',help='show plots',action='store_false')
     p.add_argument('-l','--lla',help='WGS84 lat lon [degrees] alt [meters] of observer',nargs=3,default=(None,None,None))
     p.add_argument('-k','--kmlfn',help='filename to save KML to',type=str,default=None)
-    p.add_argument('--sat',help='satellite you want to pick from file',type=int,default=None)
     a = p.parse_args()
     showplot = a.noplot
 
-    data,dates,satnum,belowhoriz,obs = loopsat(a.tlefn,a.date,a.kmlfn,a.lla,a.sat,)
-
-    #%% basic plot
+#%% setup dates
+    if len(a.date) == 1:
+        dates = [parse(a.date[0])]
+    elif len(a.date) ==2:
+        dates = date_range(start=a.date[0], end=a.date[1],
+                   freq=str(a.period)+'T')
+    else:
+        exit('specify one date, or two (start/stop) dates')
+#%% do computation
+    data,obs,belowhoriz = loopsat(a.tlefn,dates,a.kmlfn,a.lla)
+#%% basic plot
     if showplot:
-        doplot(data['lat'],data['lon'],data['az'],data['el'],dates,satnum)
+        doplot(data)
 #%% write kml
-    dokml(belowhoriz,data['lat'],data['lon'],data['alt'], obs, a.kmlfn,satnum)
+    dokml(belowhoriz,data, obs, a.kmlfn)
 #%% fancy plot
     if showplot:
-        fancyplot(data['lat'].values,data['lon'].values,dates,satnum)
+        fancyplot(data)
 
     show()

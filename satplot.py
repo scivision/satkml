d09241fde2252132af11cb40011921a657815902
scienceopen <scienceopen@users.noreply.github.com>
2015-05-27 12:19:27 -0400

    improve plotting

--- satplot.py
+++ satplot.py
@@ -16,7 +16,18 @@ from matplotlib.pyplot import figure,show
 from matplotlib.ticker import MultipleLocator
 from dateutil.parser import parse
 from re import search
-import seaborn as sns
+from warnings import warn
+from six import string_types
+try:
+    import simplekml as skml
+except ImportError as e:
+    warn('cannot write KML. pip install simplekml   {}'.format(e))
+#
+try:
+    import seaborn as sns
+    sns.set(context='talk', style='whitegrid')
+except ImportError as e:
+    warn('disabling advanced plotting. pip install seaborn   {}'.format(e))
 
 def loopsat(tlefn,dates,kmlfn,obslla):
     obs = setupobs(obslla)
@@ -30,7 +41,7 @@ def setupobs(lla):
     try:
         obs.lat = str(lla[0]); obs.lon = str(lla[1]); obs.elevation=float(lla[2])
     except ValueError:
-        print('observation location not specified. defaults to lat=0, lon=0')
+        warn('observation location not specified. defaults to lat=0, lon=0')
     return obs
 
 def compsat(tlefn,obs,dates):
@@ -59,13 +70,13 @@ def fancyplot(data):
     try:
         from mpl_toolkits.basemap import Basemap
     except ImportError as e:
-        print('could not make fancy plot.  {}'.format(e))
+        warn('could not make fancy plot.  {}'.format(e))
         return
 
     dates = data.items.values
     satnum = data.major_axis
     if dates.size>6:
-        print('skipping fancy map plot due to too many times (plot will get overly crowded)')
+        warn('skipping fancy map plot due to too many times (plot will get overly crowded)')
         return
     #lon and lat cannot be pandas Series, must be values
     for d in data:
@@ -82,8 +93,8 @@ def fancyplot(data):
         m.drawmeridians(arange(0,360,30))
         m.drawparallels(arange(-90,90,30))
         x,y = m(lon,lat)
-        m.plot(x,y,'o',color='#aaaaff',markersize=14)
-        ax.set_title('GPS constellation from {} to {}'.format(dates[0],dates[-1]))
+        m.plot(x,y,'o',color='#dadaff',markersize=14)
+        ax.set_title('GPS constellation \nfrom {} to {}'.format(dates[0],dates[-1]))
 
         for s,xp,yp in zip(satnum,x,y):
             ax.text(xp,yp,s,ha='center',va='center',fontsize=11)
@@ -110,28 +121,27 @@ def loadTLE(filename):
 
 def dokml(belowhoriz,data,obs,kmlfn):
     # TODO: make this work with multiple times properly (show animated paths)
+    # right now it just overwrites the kml file until it reaches the last time.
     if kmlfn is not None:
       try:
-          for d in data:
-              lat= data[d]['lat']; lon= data[d]['lon']
-              alt_m = data[d]['alt']
-              satnum = data[d].index
+          for t,d in data.iteritems():
+              lat= d['lat']; lon= d['lon']; alt_m = d['alt']
+              satnum = d.index
 
-              import simplekml as skml
               kmlfn = expanduser(kmlfn)
               kml1d = skml.Kml()
               for s in satnum:
                   if not belowhoriz[s]:
-                      linestr = kml1d.newlinestring(name=str(s))
+                      linestr = kml1d.newlinestring(name='PRN{:d}'.format(s))
                       linestr.coords = [(obs.lon, obs.lat, obs.elevation),
                                         (lon[s], lat[s], alt_m[s])]
                       linestr.altitudemode = skml.AltitudeMode.relativetoground
               kml1d.save(kmlfn)
       except Exception as e:
-          print('unable to write KML. Do you have simplekml package installed? ' + str(e))
+          warn('unable to write KML. Do you have simplekml package installed? ' + str(e))
 
 
-def doplot(data):
+def doplot(data,obs):
   polar = True
 
   dates = data.items
@@ -147,7 +157,7 @@ def doplot(data):
     ax1.set_xlabel('lon [deg.]')
     ax1.set_xlim(-180,180)
     ax1.set_ylim(-90,90)
-    ax1.set_title('Latitude & Longitude from\n{} to {}'.format(dates[0], dates[-1]))
+    ax1.set_title('Latitude & Longitude from\n{} to {}'.format(dates[0].strftime('%xT%X'), dates[-1].strftime('%xT%X')))
     #ax1.grid(True)
     ax1.yaxis.set_major_locator(MultipleLocator(15))
     ax1.yaxis.set_minor_locator(MultipleLocator(5))
@@ -178,11 +188,11 @@ def doplot(data):
     #ax2.grid(True)
 
     ax1.plot(lon,lat,marker='.',linestyle='-')
-    ax2.set_title('Azimuth & Elevation by PRN from\n{} to {}'.format(dates[0],dates[-1]))
+    ax2.set_title('Azimuth & Elevation by PRN @ ({},{}) from\n{} to {}'.format(obs.lat,obs.lon,dates[0],dates[-1]))
 
     if len(dates)<6: #don't want overcrowded plot
         for i,d in enumerate(dates):
-                pl = '{} {}'.format(s,d.strftime('%H:%M'))
+                pl = '{} {}'.format(s, d.strftime('%H:%M'))
                 ax1.text(lon[i]+3, lat[i]+3,pl,fontsize='x-small')
                 if isfinite(az[i]):
                     ax2.text(az[i]+azoffs, el[i],pl,fontsize='x-small')
@@ -197,7 +207,7 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='converts satellite position into KML for Google Earth viewing')
     p.add_argument('tlefn',help='file with TLE to parse',type=str)
-    p.add_argument('date',help='start/stop time to start 24 hour plot YYYY-mm-ddTHH:MM:SSZ',nargs='+',type=str)
+    p.add_argument('-d','--date',help='start/stop time to start 24 hour plot YYYY-mm-ddTHH:MM:SSZ',nargs='+',type=str,default='')
     p.add_argument('-T','--period',help='time interval (MINUTES) to compute sats. position (default=15 min)',type=str,default='15')
     p.add_argument('--noplot',help='show plots',action='store_false')
     p.add_argument('-l','--lla',help='WGS84 lat lon [degrees] alt [meters] of observer',nargs=3,default=(None,None,None))
@@ -206,18 +216,17 @@ if __name__ == '__main__':
     showplot = a.noplot
 
 #%% setup dates
-    if len(a.date) == 1:
-        dates = [parse(a.date[0])]
+    if len(a.date) == 1 or isinstance(a.date,string_types):
+        dates = [parse(a.date)]
     elif len(a.date) ==2:
-        dates = date_range(start=a.date[0], end=a.date[1],
-                   freq=str(a.period)+'T')
+        dates = date_range(start=a.date[0], end=a.date[1],  freq='{}T'.format(a.period))
     else:
-        exit('specify one date, or two (start/stop) dates')
+        raise ValueError('specify one date, or two (start/stop) dates')
 #%% do computation
     data,obs,belowhoriz = loopsat(a.tlefn,dates,a.kmlfn,a.lla)
 #%% basic plot
     if showplot:
-        doplot(data)
+        doplot(data,obs)
 #%% write kml
     dokml(belowhoriz,data, obs, a.kmlfn)
 #%% fancy plot
